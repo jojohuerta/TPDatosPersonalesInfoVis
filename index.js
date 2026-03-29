2f0fae6cd6acb361dca32bdca51e14cb397838ba
jojohuerta <johuerta@itba.edu.ar>
2026-03-29 01:26:57 -0300

    Flourish

---
+++ index.js
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Prototipo de TP - Datos Personales</title>
+</head>
+<body>
+    <h1>¿Cuánta Agua tome todos los días? (RawGraphs)</h1>
+    <object type="image/svg+xml" data="RawGraphs.svg" width="100%"></object>
+    <h1>¿Cuándo fueron los contextos donde más se tomó agua?(Flourish)</h1>
+    <div class="flourish-embed flourish-chart" data-src="visualisation/28278318"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/28278318/thumbnail" width="100%" alt="visualization" /></noscript></div>
+</body>
+</html>
