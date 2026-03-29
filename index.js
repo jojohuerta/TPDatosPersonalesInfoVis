10d686bba02e4a1301d305ea19237882eba2c4ea
jojohuerta <johuerta@itba.edu.ar>
2026-03-28 23:58:57 -0300

    Index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Prototipo de TP - Datos Personales</title>
+</head>
+<body>
+    <h1>¿Cuánta Agua tome todos los días?</h1>
+</body>
+</html>
